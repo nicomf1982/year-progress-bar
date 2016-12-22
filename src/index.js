f6d69367175ee3c53828120b7571d30e07fe5d88
nicomf1982 <nicomf1982@gmail.com>
2016-12-22 04:05:21 -0300

    initial release

---
+++ src/index.js
@@ -0,0 +1,23 @@
+'use strict';
+const chalk = require('chalk');
+
+module.exports = {
+  current: function () {
+    const initialDate = new Date(new Date().getFullYear(), 0, 1);
+    return Math.floor((((new Date() - initialDate) / (1000 * 60 * 60 * 24)) * 100) / 365);
+  },
+
+  remain: function () {
+    return 100 - parseInt(this.current())
+  },
+
+  fancy: function () {
+    let current = this.current();
+    let yearBarInit = "[0%]-["
+    let yearBar='';
+      for(let i=5; i<=100; i+=5){
+        yearBar = (i<current)?yearBar+'▓':yearBar+'░';
+      }
+    return chalk.green(yearBarInit)+chalk.yellow(yearBar)+chalk.green(`]-[${current}%]`)
+  },
+};
---
+++ test/test.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var expect = require('chai').expect;
+var yearProgress = require('../src/index');
+
+describe('#yearProgressBar', () => {
+    it('should return current year progress (%)', () => {
+        let result = yearProgress.current();
+        expect(result).to.be.a('number').and.not.to.be.NaN;
+    });
+    it('should return remain year left (%)', () => {
+      let result = yearProgress.remain();
+      expect(result).to.be.a('number').and.not.to.be.NaN;
+    });
+    it('should return fancy progress bar', () => {
+      let result = yearProgress.fancy();
+      expect(result).to.be.a('string');
+;
+
+    });
+});
